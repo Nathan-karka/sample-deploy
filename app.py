6775df4a26965d40612519be2c5acbbfcecf993d
Nathan-karka <nathanm@m.karka.academy>
2023-03-16 12:39:45 +0530

    la

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.config["JWT_COOKIE_SECURE"] = False
 app.config["JWT_SECRET_KEY"] = "Nathan04"
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(seconds=30)
 app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(minutes=1)
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:Nathan04@http://192.168.96.3:5000/karkadb"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:Nathan04@http://192.168.96.3:3306/karkadb"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JSON_SORT_KEYS'] = False
 
